93cd25fadfe3bc9cc9f5e1a3d2ece847767d4e84
Kieran <Kieranherr@gmail.com>
2020-09-24 15:17:51 -0500

    Finished palindrome

--- app.js
+++ app.js
@@ -1,52 +1,76 @@
-function fibonacci(){
-    let first = 1;
-    var second = first+first;
-    alert(first);
-    alert(second);
-    for(let i = 0; i<5; i++){
-        first = second + first;
-        alert(first);
-        second = first + second;
-        alert(second)
-    }
-}
-fibonacci();
+// function fibonacci(){
+//     let first = 1;
+//     var second = first+first;
+//     alert(first);
+//     alert(second);
+//     for(let i = 0; i<5; i++){
+//         first = second + first;
+//         alert(first);
+//         second = first + second;
+//         alert(second)
+//     }
+// }
+// fibonacci();
 
-function stringReverse(){
-    let userinput = prompt("What is the string you would like to reverse?");
-    var reverseString = "";
+// function stringReverse(){
+//     let userinput = prompt("What is the string you would like to reverse?");
+//     var reverseString = "";
+//     for(let i = userinput.length - 1; i >= 0; i--){
+//         reverseString = reverseString+userinput[i];
+//     }
+//     alert(reverseString)
+// }
+// stringReverse();
+
+// function capLetter(){
+//     let userinput = prompt("Give me a couple of words");
+//     userinput = userinput.toLowerCase();
+//     var splitString = userinput.split(' ');
+//     for(let i = 0; i < splitString.length; i++){
+//         splitString[i] = splitString[i].charAt(0).toUpperCase() + splitString[i].substring(1)
+//     }
+//     splitString = splitString.join(' ');
+//     alert(splitString);
+// }
+// capLetter();
+
+// function compressString(){
+//     let userinput = prompt("Input a string of random letters please.");
+//     var compressedString = '';
+//     var count = 0;
+//     for(let i = 0; i < userinput.length; i++){
+//         count++;
+//         if(userinput[i] != userinput[i+1]){
+//             compressedString = compressedString + userinput[i] + count;
+//             count = 0;
+//         }
+        
+        
+//     }
+//     alert(compressedString);
+// }
+// compressString();
+
+function palindrome(){
+    let userinput = prompt("Give me a word and i will tell you if it is a palindrome!");
+    var reverse = "";
     for(let i = userinput.length - 1; i >= 0; i--){
-        reverseString = reverseString+userinput[i];
+        reverse = reverse+userinput[i];
     }
-    alert(reverseString)
-}
-stringReverse();
-
-function capLetter(){
-    let userinput = prompt("Give me a couple of words");
-    userinput = userinput.toLowerCase();
-    var splitString = userinput.split(' ');
-    for(let i = 0; i < splitString.length; i++){
-        splitString[i] = splitString[i].charAt(0).toUpperCase() + splitString[i].substring(1)
-    }
-    splitString = splitString.join(' ');
-    alert(splitString);
-}
-capLetter();
-
-function compressString(){
-    let userinput = prompt("Input a string of random letters please.");
-    var compressedString = '';
-    var count = 0;
+    count = 0;
     for(let i = 0; i < userinput.length; i++){
-        count++;
-        if(userinput[i] != userinput[i+1]){
-            compressedString = compressedString + userinput[i] + count;
-            count = 0;
+        if(userinput[i] == reverse[i]){
+            count++;
         }
-        
-        
+        else{
+            continue;
+        }
     }
-    alert(compressedString);
+    if(count == userinput.length){
+        alert("It is a palindrome! :)");
+    }
+    else{
+        alert("It is not a palindrome! :(");
+    }
 }
-compressString();
+palindrome();
